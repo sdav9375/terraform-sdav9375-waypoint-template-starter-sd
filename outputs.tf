00b0829f70041faa741602135e4d289d372c9ad0
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2023-07-05 15:23:09 -0400

    Copyright

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
+# Copyright (c) HashiCorp, Inc.
+
 output "waypoint_project" {
   value = var.waypoint_project
   description = "This will only have a value if we successfully received the waypoint project name as an input"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+# Copyright (c) HashiCorp, Inc.
+
 variable "waypoint_project" {
   description = "Name of the waypoint project, to be used in all resource names."
   type        = string

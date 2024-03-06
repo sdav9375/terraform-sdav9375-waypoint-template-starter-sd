96196e037444d73085c569b18c519397631d5e0f
juliezzhou <julie.zhou@hashicorp.com>
2024-03-06 13:43:55 -0500

    add a new variable

--- variables.tf
+++ variables.tf
@@ -4,3 +4,8 @@ variable "waypoint_application" {
   description = "Name of the waypoint application, to be used in all resource names."
   type        = string
 }
+
+variable "a"{
+  description = "name"
+  type       = string
+}

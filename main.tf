ff66fd3b8eba239953a6bb12fe2d9efcba799888
Susan Davies <susan.davies@hashicorp.com>
2026-07-06 15:40:17 -0400

    add action

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 # Copyright (c) HashiCorp, Inc.
 
 resource "null_resource" "trivial_resource" {}
+
+action "aws_lambda_invoke" "api_handler" {
+  config {
+    function_name = aws_lambda_function.api_handler.function_name
+    payload = jsonencode({
+      message = "Invoke lambda from action",
+      type    = "test"
+    })
+  }
+}
